47dec884fe28efc316854d9649257aaf12dafa48
mohin556 <mohinalhaq@gmail.com>
2022-02-14 16:22:37 +0600

    fb clone first time

---
+++ src/components/Header/Header.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import './Header.css';
+import AddIcon from '@mui/icons-material/Add';
+
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import HomeIcon from '@mui/icons-material/Home';
+import FlagIcon from '@mui/icons-material/Flag';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
+import StorefrontIcon from '@mui/icons-material/Storefront';
+import ForumIcon from '@mui/icons-material/Forum';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+// import StorefrontOutlinedIcon from '@mui/icons-material/StorefrontOutlined';
+// import SupervisedUserCircleIcon from '@mui/icons-material/SupervisedUserCircle';
+
+import logo from '../../image/Group 2.png'
+import { Avatar } from '@mui/material';
+const Header = () => {
+    return (
+        <div className='header' >
+             <div className="header-left">
+                <img src={logo} alt="" />
+                <div className='header-input' >
+                <SearchIcon  />
+                   <input type="text" />
+                </div>
+             </div>
+             <div className="header-middle">
+
+                    <div className="header-option header-option-active  ">
+                     <HomeIcon fontSize='large' />
+                     
+                    </div>
+                      <div className="header-option">
+                       <FlagIcon fontSize='large' />
+                      </div>
+                       <div className="header-option">
+                          <SubscriptionsIcon fontSize='large' />
+                       </div>
+                       <div className="header-option">
+                          <StorefrontIcon fontSize='large' />
+                      </div>
+                       <div className="header-option">
+                        <SupervisedUserCircleIcon fontSize='large'  />
+                     </div>
+
+            
+
+             </div>
+             <div className="header-right">
+                <div className="header-info">
+                    <Avatar />
+                    <h4>mohinalhoque</h4>
+                </div>
+                <IconButton aria-label="add">
+                 <AddIcon />
+                </IconButton>  
+                <IconButton >
+                 <ForumIcon />
+                </IconButton>  
+                <IconButton >
+                 <NotificationsActiveIcon />
+                </IconButton> 
+
+
+                <IconButton >
+                <ExpandMoreIcon />
+                </IconButton>  
+
+
+             </div>
+        </div>
+    );
+};
+
+export default Header;
